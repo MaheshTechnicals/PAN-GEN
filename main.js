87567be6d5df1c6b80f1028153318e78ee790074
msvarma10001@gmail.com <msvarma10001@gmail.com>
2022-08-28 23:25:52 +0530

    Logic Done

--- main.js
+++ main.js
@@ -1,63 +1,51 @@
 let logo = document.getElementById("profile")
 
+let fullName = document.getElementById("left")
 
-
-
-
-
-
-
-
-
-
-
+// Fetch Data.
 
 fetch('https://randomuser.me/api/')
   .then((res) => {
     return res.json()
   }).then((data) => {
-    console.log(data)
+
 
     let fname = data.results[0].name.first
     let lname = data.results[0].name.last
     let title = data.results[0].name.title
-    
-    let proLogo= data.results[0].picture.large
-    
-    // DOM 
-    
-    
-    
-   let img =document.createElement("img")
-   img.src=proLogo
-   img.className="logo"
-   logo.append(img)
-   console.log (img)
-    
+    let proLogo = data.results[0].picture.large
 
 
     // PAN GEN
 
     let myPan = () => {
-
       let a = String.fromCharCode(Math.floor(Math.random() * 26) + 65)
       let b = String.fromCharCode(Math.floor(Math.random() * 26) + 65)
-
       let c = String.fromCharCode(Math.floor(Math.random() * 26) + 65)
-
       let d = String.fromCharCode(Math.floor(Math.random() * 26) + 65)
-
       let e = lname[0]
-
       let f = Math.floor(Math.random() * (9999 - 1000) + 1000)
-
       let g = lname.slice(-1).toUpperCase()
-
-
       let panNum = a + b + c + d + e + f + g
-      console.log(panNum)
+
+
+
+      // DOM 
+
+
+      let img = document.createElement("img")
+      img.src = proLogo
+      img.className = "logo"
+      logo.append(img)
+
+      let display = document.createElement("p")
+      display.innerText = title + " " + fname + " " + lname
+      fullName.append(display)
+
+      let Pan = document.createElement("p")
+      Pan.innerText = `PAN: ${panNum} `
+      display.appendChild(Pan)
 
     }
-
     myPan()
   })
